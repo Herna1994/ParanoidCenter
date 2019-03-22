73c33c6c7177def7821c4be50bc8ecd3cee5c5bd
Anay Wadhera <anay1018@gmail.com>
2019-03-21 19:10:38 -0700

    mata picks

--- template.sh
+++ template.sh
@@ -4,4 +4,7 @@
 # If possible starting after vendor/pa one, categorize them by alphabetical order of repositories name.
 
 # <RepositoryName>
-repo download -c repository/path gerritCommitNumber/patchset # Short info or commit title.
+repo download -c hardware/qcom/audio   5477/1 # audio_extn: Add missing pthread.h includes
+repo download -c hardware/qcom/display 5475/2 # sdm: add board flag for panel shift
+repo download -c vendor/codeaurora/commonsys/telephony 5306/1 # [DNM] telephony: HACK
+repo download -c vendor/pa 5343/9 # vendor: add mata
